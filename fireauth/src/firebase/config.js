374edaea28df65d060909c6e85f4cf5a87146497
wooluck210 <54842493+wooluck210@users.noreply.github.com>
2023-10-11 23:36:57 +0900

    1011 firbase 학습 및 화면 렌더링 문제

---
+++ fireauth/src/firebase/config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+import {getAuth} from 'firbase/auth'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const appFireStore = getFirestore(app)
+
+const appAuth = getAuth()
+
+export {appFireStore, appAuth}
